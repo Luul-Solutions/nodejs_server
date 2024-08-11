43f1ab3c9dfebc929d5e2270178940172c2c891c
= <=>
2024-08-11 14:18:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const http = require("http")
+const express = require("express")
+const bodyParser = require("body-parser")
+// http request
+// http response
+const app = express()
+
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use("/admin", adminRoutes)
+app.use(shopRoutes)
+app.use(
+    (req, res, next) => {
+        res.status(404).send("<h1>Page not found</h1>")
+
+    });
+
+
+
+
+
+app.listen(3000)
+
+
+
+
+
---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+
+const express = require("express")
+
+const router = express.Router()
+const path = require("path")
+const rootDir = require('../utils/path')
+
+router.get("/add-product", (req, res) => {
+    res.sendFile(path.join(rootDir, "views", "add-product.html"))
+})
+
+router.post("/product", (req, res) => {
+    console.log(req.body)
+    console.log("Product added")
+    res.redirect("/")
+})
+module.exports = router;
+
