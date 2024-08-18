dbcaf5941c08abf70d279ed9e201f5072f9c3af9
Salbtw <salbtw671@gmail.com>
2024-08-18 18:46:03 +0300

    Salman's changes

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-const http = require("http")
-const express = require("express")
-const bodyParser = require("body-parser")
-const path = require("path")
-const rootDir = require('./util/path');
-// http request
-// http response
-const app = express()
+const path = require('path');
 
-const adminRoutes = require("./routes/admin")
-const shopRoutes = require("./routes/shop")
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
+const app = express();
 
+app.engine(
+  'hbs',
+  expressHbs.engine({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, "public")))
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use("/admin", adminRoutes)
-app.use(shopRoutes)
-app.use(
-    (req, res, next) => {
-        res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    });
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-
-
-
-app.listen(3000)
+app.listen(3000);
